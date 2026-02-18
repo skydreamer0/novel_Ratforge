976ee3391638e07498e6b4b095c3928d7b878f46
George <skyaski00@gmail.com>
2026-02-18 20:02:25 +0800

    TTS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ratforge-reader-v3';
+const CACHE_NAME = 'ratforge-reader-v4';
 const ASSETS = [
     './',
     './index.html',
@@ -8,10 +8,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
         })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
